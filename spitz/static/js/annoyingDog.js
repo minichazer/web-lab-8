e537a8a1f4057437ecb19ed252e2201fd54eadba
minichazer <minichazer@gmail.com>
2022-12-20 21:09:50 +0500

    Optimize design for lowres screens

--- spitz/static/js/annoyingDog.js
+++ spitz/static/js/annoyingDog.js
@@ -5,8 +5,12 @@ annoyingDog.hover(function(){
   annoyingDogAudio.play();
   $('.pet-me').hide();
   $('.pet-me2').show();
+  $('.pet-me3').hide();
+  $('.pet-me4').show();
 }, function(){
   annoyingDogAudio.pause();
   $('.pet-me').show();
   $('.pet-me2').hide();
+  $('.pet-me3').show();
+  $('.pet-me4').hide();
 });
